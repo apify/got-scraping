9b005d9cbba121737ff2aeb0f6f78d447a4778a5
Petr Patek <petrpatekml@gmail.com>
2021-03-17 16:48:34 +0100

    Index - added comments.

--- src/index.js
+++ src/index.js
@@ -8,12 +8,16 @@ const { browserHeadersHandler } = require('./handlers/browser-headers');
 const { proxyHandler } = require('./handlers/proxy');
 
 const mutableGot = got.extend({
+    // Must be mutable in order to override the defaults
+    // https://github.com/sindresorhus/got#instances
     mutableDefaults: true,
     context: {
+        // Custom header generator instance.
         headerGenerator: new HeaderGenerator(),
     },
 });
 
+// Overriding the mutableGot defaults by merging its defaults and our scraping defaults.
 mutableGot.defaults.options = got.mergeOptions(mutableGot.defaults.options, SCRAPING_DEFAULT_OPTIONS);
 
 const gotScraping = got.extend(
