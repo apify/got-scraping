cac26de6a31b7cffb6473fbd8db6a16ea1935175
Petr Patek <petrpatekml@gmail.com>
2021-04-07 09:46:58 +0200

    Unhandled exceptions - added request kill

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ const { customOptionsHandler } = require('./handlers/custom-options');
 const { browserHeadersHandler } = require('./handlers/browser-headers');
 const { proxyHandler } = require('./handlers/proxy');
 
+const isResponseOk = (response) => {
+    const { statusCode } = response;
+    const limitStatusCode = response.request.options.followRedirect ? 299 : 399;
+
+    return (statusCode >= 200 && statusCode <= limitStatusCode) || statusCode === 304;
+};
+
 const mutableGot = got.extend({
     // Must be mutable in order to override the defaults
     // https://github.com/sindresorhus/got#instances
@@ -15,6 +22,18 @@ const mutableGot = got.extend({
     context: {
         // Custom header generator instance.
         headerGenerator: new HeaderGenerator(),
+    },
+    // Got has issues with terminating requests and it can cause unhandled exceptions
+    hooks: {
+        afterResponse: [
+            (response) => {
+                if (isResponseOk(response)) {
+                    response.request.destroy();
+                }
+
+                return response;
+            },
+        ],
     },
 });
 
