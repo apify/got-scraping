1f45cfd70c8a83fd3ddc029b13e2047f6d3b5699
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2021-08-10 10:33:47 +0200

    comment

--- src/hooks/insecure-parser.js
+++ src/hooks/insecure-parser.js
@@ -2,6 +2,8 @@
 
 const insecureParserHook = (options) => {
     if (options.context.insecureHTTPParser !== undefined) {
+        // `insecureHTTPParser` is not an allowed HTTP option in Got,
+        // so we must inject it directly into the Got internals for Node.js `http` module to use it.
         options._unixOptions = {
             ...options._unixOptions,
             insecureHTTPParser: options.context.insecureHTTPParser,
