63fff5b4bab1e21cbbbc67d5ba9e859c828d83c2
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2021-08-16 11:31:04 +0200

    fixup

--- src/hooks/http2.ts
+++ src/hooks/http2.ts
@@ -4,7 +4,6 @@ import { auto } from 'http2-wrapper';
 
 export function http2Hook(options: Options): void {
     if (options.http2 && (options.url as URL).protocol !== 'http:') {
-        // @ts-expect-error FIXME
-        options.request = auto;
+        options.request = auto as typeof options.request;
     }
 }
