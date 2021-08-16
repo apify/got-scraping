71a37aba9426dec74db2461a0d41359008b6f21a
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2021-08-16 11:37:05 +0200

    fixup

--- src/hooks/http2.ts
+++ src/hooks/http2.ts
@@ -1,9 +1,9 @@
 import { URL } from 'url';
-import { Options } from 'got-cjs';
+import { Options, RequestFunction } from 'got-cjs';
 import { auto } from 'http2-wrapper';
 
 export function http2Hook(options: Options): void {
     if (options.http2 && (options.url as URL).protocol !== 'http:') {
-        options.request = auto as typeof options.request;
+        options.request = auto as RequestFunction;
     }
 }
