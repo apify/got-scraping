c2fa526399612937856d4270334608143c0e976e
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2021-08-25 17:19:18 +0200

    move some tls defaults into hooks/tls

--- src/hooks/tls.ts
+++ src/hooks/tls.ts
@@ -1,7 +1,104 @@
 import { Options } from 'got-cjs';
 // @ts-expect-error Missing types
 import { getBrowser, getUserAgent } from 'header-generator/src/utils';
-import { knownCiphers, sigalgs, minVersion, maxVersion } from '../scraping-defaults';
+
+// @ts-expect-error Custom ECDH curves are not used yet as Node.js doesn't support some of them.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const ecdhCurve = {
+    firefox: [
+        'X25519',
+        // 'secp256r1',
+        'secp384r1',
+        'secp521r1',
+        // 'ffdhe2048',
+        // 'ffdhe3072',
+    ].join(':'),
+    chrome: [
+        'X25519',
+        // 'secp256r1',
+        'secp384r1',
+    ].join(':'),
+} as const;
+
+export const sigalgs = {
+    firefox: [
+        'ecdsa_secp256r1_sha256',
+        'ecdsa_secp384r1_sha384',
+        'ecdsa_secp521r1_sha512',
+        'rsa_pss_rsae_sha256',
+        'rsa_pss_rsae_sha384',
+        'rsa_pss_rsae_sha512',
+        'rsa_pkcs1_sha256',
+        'rsa_pkcs1_sha384',
+        'rsa_pkcs1_sha512',
+        'ECDSA+SHA1',
+        'rsa_pkcs1_sha1',
+    ].join(':'),
+    chrome: [
+        'ecdsa_secp256r1_sha256',
+        'rsa_pss_rsae_sha256',
+        'rsa_pkcs1_sha256',
+        'ecdsa_secp384r1_sha384',
+        'rsa_pss_rsae_sha384',
+        'rsa_pkcs1_sha384',
+        'rsa_pss_rsae_sha512',
+        'rsa_pkcs1_sha512',
+    ].join(':'),
+} as const;
+
+export const knownCiphers = {
+    chrome: [
+        // Chrome v92
+        'TLS_AES_128_GCM_SHA256',
+        'TLS_AES_256_GCM_SHA384',
+        'TLS_CHACHA20_POLY1305_SHA256',
+        'ECDHE-ECDSA-AES128-GCM-SHA256',
+        'ECDHE-RSA-AES128-GCM-SHA256',
+        'ECDHE-ECDSA-AES256-GCM-SHA384',
+        'ECDHE-RSA-AES256-GCM-SHA384',
+        'ECDHE-ECDSA-CHACHA20-POLY1305',
+        'ECDHE-RSA-CHACHA20-POLY1305',
+        // Legacy:
+        'ECDHE-RSA-AES128-SHA',
+        'ECDHE-RSA-AES256-SHA',
+        'AES128-GCM-SHA256',
+        'AES256-GCM-SHA384',
+        'AES128-SHA',
+        'AES256-SHA',
+    ].join(':'),
+    firefox: [
+        // Firefox v91
+        'TLS_AES_128_GCM_SHA256',
+        'TLS_CHACHA20_POLY1305_SHA256',
+        'TLS_AES_256_GCM_SHA384',
+        'ECDHE-ECDSA-AES128-GCM-SHA256',
+        'ECDHE-RSA-AES128-GCM-SHA256',
+        'ECDHE-ECDSA-CHACHA20-POLY1305',
+        'ECDHE-RSA-CHACHA20-POLY1305',
+        'ECDHE-ECDSA-AES256-GCM-SHA384',
+        'ECDHE-RSA-AES256-GCM-SHA384',
+        // Legacy:
+        'ECDHE-ECDSA-AES256-SHA',
+        'ECDHE-ECDSA-AES128-SHA',
+        'ECDHE-RSA-AES128-SHA',
+        'ECDHE-RSA-AES256-SHA',
+        'AES128-GCM-SHA256',
+        'AES256-GCM-SHA384',
+        'AES128-SHA',
+        'AES256-SHA',
+        'DES-CBC3-SHA',
+    ].join(':'),
+} as const;
+
+export const minVersion = {
+    firefox: 'TLSv1.2',
+    chrome: 'TLSv1.0',
+} as const;
+
+export const maxVersion = {
+    firefox: 'TLSv1.3',
+    chrome: 'TLSv1.3',
+} as const;
 
 type Browser = 'chrome' | 'firefox' | 'safari' | undefined;
 
