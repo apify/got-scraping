4684bf2d3b1f0b5b2612bbdcc57fb6f7b3987ad0
Vlad Frangu <kingdgrizzle@gmail.com>
2023-11-16 15:07:09 +0200

    fix: compile module in one file and add missing ignores

---
+++ tsup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    entry: ['src/index.ts'],
+    external: [],
+    noExternal: [],
+    platform: 'node',
+    format: ['esm'],
+    target: 'es2022',
+    skipNodeModulesBundle: true,
+    clean: true,
+    minify: false,
+    terserOptions: {
+        mangle: false,
+        keep_classnames: true,
+        keep_fnames: true,
+    },
+    splitting: false,
+    keepNames: true,
+    dts: true,
+    sourcemap: true,
+    treeshake: false,
+    outDir: 'dist',
+});
